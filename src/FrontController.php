84a7bf0a6e29a362ce59a029a4745e76ceba257d
Petr Fořt <p.fort1990@gmail.com>
2024-10-24 19:34:33 +0200

    Uprava logiky FrontController - dynamické načítání kontrolérů

--- src/FrontController.php
+++ src/FrontController.php
@@ -6,18 +6,32 @@ namespace Pfort\Blog;
 use Pfort\Blog\App\Container\ContainerInterface;
 use Pfort\Blog\App\Http\Request;
 use Pfort\Blog\App\Router\RouterInterface;
+use Pfort\Blog\Dispatcher\EventDispatcher;
 use Pfort\Blog\Factories\RoutesFactory;
 
 readonly final class FrontController
 {
+    private const CONTROLLERS_NAMESPACE = 'Pfort\\Blog\\Controllers';
+
     public function __construct(
         private ContainerInterface $container,
         private RouterInterface $router,
         private Request $request,
+        private EventDispatcher $dispatcher
     ) {}
 
     public function run(): void {
         $route = $this->router->match($this->request, RoutesFactory::create());
-        var_dump($route);
+
+        $controller = $route->getHandler();
+
+        $controller = explode('::', $controller);
+
+        $controllerClass = self::CONTROLLERS_NAMESPACE . "\\" . $controller[0];
+        $controllerMethod = $controller[1];
+
+        $instance = new $controllerClass();
+        $instance->execute($this->request, $controllerMethod, $route->getParams());
+
     }
 }
