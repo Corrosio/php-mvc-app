320a95cba8692c0e5829e56bb7af0bc68ccfefd4
Petr Fořt <p.fort1990@gmail.com>
2024-10-24 19:32:03 +0200

    Oprava definice služeb v kontejneru

--- src/Factories/DiContainerFactory.php
+++ src/Factories/DiContainerFactory.php
@@ -9,6 +9,8 @@ use Pfort\Blog\App\Http\Request;
 use Pfort\Blog\App\Router\DynamicRouterDecorator;
 use Pfort\Blog\App\Router\Router;
 use Pfort\Blog\Config\Configuration;
+use Pfort\Blog\Dispatcher\DispatcherFactory;
+use Pfort\Blog\Dispatcher\EventDispatcher;
 use Pimple\Container;
 
 final class DiContainerFactory
@@ -17,14 +19,16 @@ final class DiContainerFactory
     {
         return new DiContainer(new Container([
 
-            'config' => fn () => new Configuration(
+            Configuration::class => fn () => new Configuration(
                 ROOT_DIR . '/dev_config.php',
                 ROOT_DIR . '/prod_config.php'
             ),
 
+            EventDispatcher::class => fn () => DispatcherFactory::create(),
+
             'router' => fn () => new Router(),
 
-            'request' => function(Container $container) {
+            Request::class => function(Container $container) {
                 return new Request($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
             },
 
